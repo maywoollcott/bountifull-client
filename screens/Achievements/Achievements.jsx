4d1e8fd2c806d071da4a35114a860acfd0fe9a5a
maywoollcott <maywoollcott@gmail.com>
2021-03-30 14:51:06 -0400

    adds all achievements logic

--- screens/Achievements/Achievements.jsx
+++ screens/Achievements/Achievements.jsx
@@ -1,11 +1,132 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 import { AntDesign, Entypo, FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import styles from './Achievements.style';
 import {COLORS} from '../../globalStyles';
+import * as SecureStore from 'expo-secure-store';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 
 export default function Achievements() {
+
+  const { _id } = useSelector(state => state.user);
+  const dateToday = new Date().toISOString().substring(0, 10);
+
+  const API_URL = 'http://192.168.1.148:3001';
+
+  const [days, setDays] = useState([]);
+  const [streak, setStreak] = useState(0);
+  const [streakRecord, setStreakRecord] = useState(0);
+  const [singleDayHundredTrophy, setSingleDayHundredTrophy] = useState(false);
+  const [thirtyDayHundredTrophy, setThirtyDayHundredTrophy] = useState(false);
+
+  useEffect( () => {
+    getDays();
+  }, [])
+
+  useEffect( () => {
+    calculateCurrentStreak();
+    calculateRecordStreak();
+    checkSingleDayHundredTrophy();
+    checkThirtyDayHundredTrophy();
+  }, [days])
+  
+  const getDays = async () => {
+    const token = await SecureStore.getItemAsync('BOUNTIFULL_TOKEN_AUTH');
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+      const { data } = await axios.get(`${API_URL}/userdays/${_id}`, config);
+      console.log(data)
+      setDays(data)
+    } catch (error) {
+      console.log('error ', error)
+    }
+  }
+
+  const calculateCurrentStreak = () => {
+    const activeDays = days.filter(day => {
+      return day.totalGoalMet > 0;
+    })
+    const activeDates = activeDays.map(day => {
+      return Date.parse(day.date);
+    })
+
+    let currentStreak = 0
+
+    if (activeDates[0] === Date.parse(dateToday) || activeDates[0] === Date.parse(dateToday) - 86400000) {
+      currentStreak = 1
+      for (let i = 0; i < activeDates.length; i++) {
+        if (activeDates[i] - activeDates[i+1] === 86400000) {
+          currentStreak ++
+        } else {
+          break;
+        }
+      }
+    };
+    setStreak(currentStreak);
+  }
+
+  const calculateRecordStreak = () => {
+    const activeDays = days.filter(day => {
+      return day.totalGoalMet > 0;
+    })
+    const activeDates = activeDays.map(day => {
+      return Date.parse(day.date);
+    })
+
+    if (activeDates.length > 0) {
+      let longestConsecutive = 1
+      let currentConsecutive = 1
+      for (let i = 0; i < activeDates.length; i++) {
+        if (activeDates[i] - activeDates[i+1] === 86400000) {
+          currentConsecutive ++
+          if (currentConsecutive > longestConsecutive) {
+            longestConsecutive = currentConsecutive
+          };
+        } else {
+          currentConsecutive = 1
+        }
+      }
+      setStreakRecord(longestConsecutive);
+    }
+  }
+
+  const checkSingleDayHundredTrophy = () => {
+    const hundredDays = days.filter(day => {
+      return day.totalGoalMet >= 100;
+    })
+    if (hundredDays.length > 0) setSingleDayHundredTrophy(true);
+  }
+
+  const checkThirtyDayHundredTrophy = () => {
+    const hundredDays = days.filter(day => {
+      return day.totalGoalMet >= 100;
+    })
+    const hundredDates = hundredDays.map(day => {
+      return Date.parse(day.date);
+    })
+    console.log(hundredDates)
+    if (hundredDates.length >= 30) {
+      let longestConsecutive = 1
+      let currentConsecutive = 1
+      for (let i = 0; i < hundredDates.length; i++) {
+        if (hundredDates[i] - hundredDates[i+1] === 86400000) {
+          currentConsecutive ++
+          if (currentConsecutive > longestConsecutive) {
+            longestConsecutive = currentConsecutive
+          };
+        } else {
+          currentConsecutive = 1
+        }
+      };
+      if (longestConsecutive >= 30) setThirtyDayHundredTrophy(true);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -34,11 +155,11 @@ export default function Achievements() {
             color={COLORS.darkblue}
           />
           <View style={styles.streakTextContainer}>
-            <Text style={styles.h1}>24</Text>
+            <Text style={styles.h1}>{streak}</Text>
             <Text style={styles.h2}>CURRENT STREAK</Text>
           </View>
           <View style={styles.streakTextContainer}>
-            <Text style={styles.h1}>32</Text>
+            <Text style={styles.h1}>{streakRecord}</Text>
             <Text style={styles.h2}>RECORD STREAK</Text>
           </View>
         </View>
@@ -48,7 +169,7 @@ export default function Achievements() {
           <MaterialIcons
             name='flash-on'
             size={80}
-            color={COLORS.gray}
+            color={streakRecord >= 30 ? COLORS.turq : COLORS.gray}
           />
           <Text style={styles.h3}>Log your food for 30 days in a row</Text>
         </View>
@@ -56,7 +177,7 @@ export default function Achievements() {
           <MaterialIcons
             name='auto-awesome'
             size={80}
-            color={COLORS.gray}
+            color={streakRecord >= 60 ? COLORS.turq : COLORS.gray}
           />
           <Text style={styles.h3}>Log your food for 60 days in a row</Text>
         </View>
@@ -66,7 +187,7 @@ export default function Achievements() {
           <MaterialIcons
             name='stars'
             size={80}
-            color={COLORS.turq}
+            color={singleDayHundredTrophy ? COLORS.turq : COLORS.gray}
           />
           <Text style={styles.h3}>Meet 100% of requirements in a single day</Text>
         </View>
@@ -74,7 +195,7 @@ export default function Achievements() {
           <MaterialIcons
             name='military-tech'
             size={80}
-            color={COLORS.gray}
+            color={thirtyDayHundredTrophy ? COLORS.turq : COLORS.gray}
           />
           <Text style={styles.h3}>Meet 100% of your requirements for 30 days in a row</Text>
         </View>
